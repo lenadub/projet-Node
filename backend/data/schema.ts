53bcdaa1a4fa5b02572485620dedafb9c6ca08cc
lenadub <lena_dubois@orange.fr>
2024-12-25 17:43:40 +0100

    Avoid error for Top-level await expressions by wrapping await functions into an async function

--- backend/data/schema.ts
+++ backend/data/schema.ts
@@ -1,27 +1,27 @@
-import pool from "./connect.js"
+import pool from "./connect"
 
 // create table 'users'
 const createUsers = `
-  create table if not exists users(
-    id SERIAL primary key,
-    username TEXT not null unique,
-    email TEXT not null
-  )
+    create table if not exists users(
+                                        id SERIAL primary key,
+                                        username TEXT not null unique,
+                                        email TEXT not null
+    )
 `
 
 // create table 'books'
 const createBooks = `
-  create table if not exists books(
-    id SERIAL primary key,
-    title TEXT not null,
-    author TEXT not null,
-    editor TEXT not null,
-    year INTEGER not null,
-    price REAL not null,
-    description TEXT, 
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP
-  )
+    create table if not exists books(
+                                        id SERIAL primary key,
+                                        title TEXT not null,
+                                        author TEXT not null,
+                                        editor TEXT not null,
+                                        year INTEGER not null,
+                                        price REAL not null,
+                                        description TEXT,
+                                        created_at TIMESTAMP,
+                                        updated_at TIMESTAMP
+    )
 `
 // create table shopping 'carts'
 // normally one per user, if users are managed by the app, or only 1 cart if no users
@@ -42,9 +42,12 @@ const createOrderItems = `
 // create table  PDF 'invoices'
 const createInvoices = `
   // to be defined
-`
+`;
 
-await pool.query(createUsers)
-await pool.query(createBooks)
-
-await pool.end()
+// To avoid error for  Top-level await expressions
+// we wrap this into an async function
+(async () => {
+    await pool.query(createUsers);
+    await pool.query(createBooks);
+    await pool.end();
+})();
