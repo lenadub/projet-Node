149ec2d8b0cce3ea805732bf9655f66e33664d06
lenadub <lena_dubois@orange.fr>
2024-12-26 18:48:57 +0100

    merge from dev_sarah

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,23 @@
 import express from "express"
 import { findBook, findBookByTitle, showBooks, createBook } from "./data/queries"
 import {router} from "./data/routes"
+import cors from "cors";
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
 let app = express();
 const PORT = 3000
+
+const corsOptions = {
+  origin: '*', // Permet toutes les origines
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Méthodes HTTP autorisées
+  allowedHeaders: ['Content-Type', 'Authorization'], // En-têtes autorisés
+};
+
+// Appliquer le middleware CORS
+app.use(cors(corsOptions));
+
+app.use(express.static('public'));
 
 app.use(express.json());
 app.use('/', router);
@@ -20,13 +32,60 @@ const swaggerOptions = {
       description: 'API for managing products',
     },
   },
-  apis: ['./data/routes.ts'], // Path to the file with Swagger annotations
+  apis: ['./route.ts'], // Path to the file with Swagger annotations
 };
-
 
 const swaggerSpec = swaggerJSDoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+
+// // Define home route
+// app.get('/', (req, res) => {
+//   console.log("  Request GET /")
+//   res.status(200)
+//   res.json({"status": 200, "message": "API server OK"})
+// })
+//
+// app.get("/books", async (req, res) => {
+//   let books = await showBooks()
+//   console.log("  Request GET /books")
+//   // console.log(`  fetched books: ${JSON.stringify(books)}`)
+//   res.status(200)
+//   res.json(books)
+// })
+//
+// app.get("/books/search", async (req, res) => {
+//   let book = await findBookByTitle(req.query.title)
+//   console.log(`  Request GET /books/search with title: ${req.query.title}`)
+//   res.status(200)
+//   res.json(book)
+// })
+//
+//
+// app.get("/books/:id", async (req, res) => {
+//   let book = await findBook(req.params.id)
+//   console.log(`  Request GET /books/:id with param id: ${req.params.id}`)
+//   res.status(200)
+//   res.json(book)
+// })
+//
+// // Route to create books from the portal
+// // To be used by the ecommerce site admin
+// app.post("/books", async (req, res) => {
+//   let book = await createBook(req.body)
+//    console.log(`  Request POST /books/ with param id: ${req.params.id}`)
+//    res.status(201)
+//    res.json(book)
+//  })
+//
+//
+
+// Prevent CORS errors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Origin")
+  next();
+})
 
 //Start API server
 let server = app.listen(PORT, () => {
