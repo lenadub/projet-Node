e79176f867955b40e407b9d8069e62d3bf63633e
lenadub <lena_dubois@orange.fr>
2024-12-26 10:46:52 +0100

    new version to make test calls work

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    transform: {'^.+\\.ts?$': 'ts-jest'},
+    preset: 'ts-jest',
     testEnvironment: 'node',
-    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+    testMatch: ['**/tests/*.ts'], // Match directly inside the 'tests' folder
 };
