d70c653086da1c44b64d94723e1e89e58417f1db
lenadub <145775374+lenadub@users.noreply.github.com>
2024-12-27 21:50:50 +0100

    Update connect.ts
    updated comments

--- backend/data/connect.ts
+++ backend/data/connect.ts
@@ -2,10 +2,12 @@
 // Had to amend the import for the unitary tests to work
 // for more detail, see : https://stackoverflow.com/questions/71055340/getting-undefined-import-of-postgres-in-jest
 // previous import was : import pg from "pg"
+// pg used to manage psql database
 import * as pg from 'pg';
 
 const { Pool } = pg
 
+// db and credentials
 const dbConfig = {
 	user: 'demo',
 	password: 'demo',
