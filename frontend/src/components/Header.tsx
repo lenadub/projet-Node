9ce3c3ae448b628e1956af9aa96d7efe736bd466
lenadub <145775374+lenadub@users.noreply.github.com>
2024-12-28 22:15:46 +0100

    Update Header.tsx
    update comments

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,5 @@
 // import classes from '../styles/Header.module.css'
+// Importing necessary hooks from React
 //
 // function Header() {
 //   return(
@@ -8,38 +9,52 @@
 //
 // export default Header
 
+// Importing necessary hooks from React
 import { useState, useEffect } from "react";
+// Importing CSS module for styling the Header component
 import classes from '../styles/Header.module.css';
 
+// Defining the Header functional component
 function Header() {
+  // State to hold the username, initialized to null
   const [username, setUsername] = useState<string | null>(null);
 
+  // useEffect hook to get user data when the component mounts
   useEffect(() => {
+    //  function to get user data
     const fetchUser = async () => {
       try {
-        const response = await fetch("http://localhost:3000/users/1"); // Replace 1 with dynamic user ID if needed
+        // Fetching user data from backend API 
+        // UserId to be updated when users are handled by frontend
+        const response = await fetch("http://localhost:3000/users/1"); 
 
         if (!response.ok) {
-          throw new Error("Failed to fetch user data.");
+          throw new Error("Failed to fetch user data."); 
         }
 
         const userData = await response.json();
+        // Updating the username state with the retrieved  username
         setUsername(userData.username);
       } catch (error) {
         console.error(error);
       }
     };
 
+    // Calling the fetchUser function to initiate the data fetching
     fetchUser();
   }, []);
 
   return (
       <div className={classes.header}>
         <div>BookShelf Store</div>
+        {/* Displaying the username or a loading message if the username is not yet available */}
         <div className={classes.username}>username: {username ? username : "Loading..."}</div>
       </div>
   );
 }
 
+// Exporting the Header component for use in other parts of the application
 export default Header;
+
+
 
