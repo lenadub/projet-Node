1ef41b6a6fb67344cf115df21ba48da6c87af5af
lenadub <lena_dubois@orange.fr>
2024-12-28 19:34:42 +0100

    new header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,45 @@
-import classes from '../styles/Header.module.css'
+// import classes from '../styles/Header.module.css'
+//
+// function Header() {
+//   return(
+//     <div className={classes.header}>header</div>
+//   )
+// }
+//
+// export default Header
+
+import { useState, useEffect } from "react";
+import classes from '../styles/Header.module.css';
 
 function Header() {
-  return(
-    <div className={classes.header}>header</div>
-  )
+  const [username, setUsername] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/users/1"); // Replace 1 with dynamic user ID if needed
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch user data.");
+        }
+
+        const userData = await response.json();
+        setUsername(userData.username);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  return (
+      <div className={classes.header}>
+        <div>BookShelf Store</div>
+        <div className={classes.username}>username: {username ? username : "Loading..."}</div>
+      </div>
+  );
 }
 
-export default Header
+export default Header;
+
