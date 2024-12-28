a4db496be781b361606ed3e9652f3ea99bdb0a7f
lenadub <lena_dubois@orange.fr>
2024-12-28 19:34:13 +0100

    typed function and const

--- backend/data/queries.ts
+++ backend/data/queries.ts
@@ -5,12 +5,13 @@
 
 // import psql pool
 import pool from "./connect";
+import { QueryResult } from 'pg';
 
 /////// USERS //////
 // Creates a new user in the database with provided credentials
 // Returns the newly created user's ID
 
-interface User {
+export interface User {
   id: number;
   username: string;
   password: string;
@@ -19,12 +20,12 @@ interface User {
 
 
 async function createUser(username: string, password: string, email: string): Promise<{ id: number }> {
-  const insert = `
+  const insert:string = `
     insert into users(username,password, email)
     values($1, $2, $3) RETURNING id
   `;
-  const values = [username, password, email];
-  let response = await pool.query(insert, values);
+  const values:string[] = [username, password, email];
+  const response:QueryResult = await pool.query(insert, values);
 
   // Return rows[0]. Should include user id if no error
   return response.rows[0];
@@ -33,11 +34,11 @@ async function createUser(username: string, password: string, email: string): Pr
 // Retrieves user information by user ID
 // Throws error if user is not found
 async function findUser(userId: number): Promise<User> {
-  const select = `
+  const select:string = `
     select * from users where id=$1
   `;
-  const values = [userId];
-  const response = await pool.query(select, values);
+  const values:number[] = [userId];
+  const response:QueryResult = await pool.query(select, values);
   if (response.rows.length === 0) {
     throw new Error("User not found");
   }
@@ -48,11 +49,11 @@ async function findUser(userId: number): Promise<User> {
 // Searches for a user by  username
 // Throws error if user is not found
 async function findUserbyName(userName: string): Promise<User> {
-  const select = `
+  const select:string= `
     select * from users where username=$1
   `;
-  const values = [userName];
-  const response = await pool.query(select, values);
+  const values:string[] = [userName];
+  const response:QueryResult = await pool.query(select, values);
   if (response.rows.length === 0) {
     throw new Error("User not found");
   }
@@ -63,13 +64,13 @@ async function findUserbyName(userName: string): Promise<User> {
 // Updates user's password in the database
 // Throws error if user not found or update fails
 async function updateUserPassword(userId: number, newPassword: string): Promise<void> {
-  const updateQuery = `
+  const updateQuery:string = `
     update users
     set password = $2
     where id = $1
   `;
-  const values = [userId, newPassword];
-  const response = await pool.query(updateQuery, values);
+  const values:(string|number)[] = [userId, newPassword];
+  const response:QueryResult = await pool.query(updateQuery, values);
   if (response.rowCount === 0) {
     throw new Error("User not found or password update failed");
   }
@@ -80,10 +81,10 @@ async function updateUserPassword(userId: number, newPassword: string): Promise<
 
 // Removes a user from the database by their ID
 async function deleteUser(userId: number): Promise<void> {
-  const deleteQuery = `
+  const deleteQuery:string = `
     delete from users where id=$1
   `;
-  const values = [userId];
+  const values:number[] = [userId];
   await pool.query(deleteQuery, values);
 }
 
@@ -92,7 +93,7 @@ async function deleteUser(userId: number): Promise<void> {
 // Automatically sets creation and update timestamps
 // Returns the book's reference number
 
-interface Book {
+export interface Book {
   reference: number;
   title: string;
   author: string;
@@ -106,6 +107,8 @@ interface Book {
   updated_at: Date;
 }
 
+// the call argument was inspired from :
+// https://bobbyhadz.com/blog/typescript-object-destructuring-function-parameters
 async function createBook({
                             reference,
                             title,
@@ -133,7 +136,7 @@ async function createBook({
   `;
   const currentDate = new Date();
   const values = [reference, title, author, editor, year, price, description, cover, stock, currentDate, currentDate];
-  let response = await pool.query(insert, values);
+  const response:QueryResult = await pool.query(insert, values);
 
   // Return rows[0]. Should include book reference if no error
   return response.rows[0];
@@ -141,8 +144,8 @@ async function createBook({
 
 // Retrieves book information by reference number
 async function findBook(bookRef: number): Promise<Book> {
-  const select = `select * from books where books.reference='${bookRef}'`;
-  const response = await pool.query(select);
+  const select:string = `select * from books where books.reference='${bookRef}'`;
+  const response:QueryResult = await pool.query(select);
 
   // Return rows[0]. Should include book data if no error
   return response.rows[0];
@@ -151,8 +154,8 @@ async function findBook(bookRef: number): Promise<Book> {
 // Searches for books by title (case-insensitive partial match)
 // Returns array of matching books
 async function findBookByTitle(bookTitle: string): Promise<Book[]> {
-  const select = `select * from books where LOWER(books.title) like LOWER('%${bookTitle}%')`;
-  const response = await pool.query(select);
+  const select:string = `select * from books where LOWER(books.title) like LOWER('%${bookTitle}%')`;
+  const response:QueryResult = await pool.query(select);
   // console.log(JSON.stringify(response.rows));
 
   // Return rows[0]. Should include book data if no error
@@ -161,24 +164,26 @@ async function findBookByTitle(bookTitle: string): Promise<Book[]> {
 
 // Removes a book from database by reference number
 async function deleteBook(bookRef: number): Promise<void> {
-  const deleteQuery = `
+  const deleteQuery:string = `
     delete from books where reference=$1
   `;
-  const values = [bookRef];
+  const values:number[] = [bookRef];
   await pool.query(deleteQuery, values);
 }
 
 // Removes books from database matching partial title
 async function deleteBookByTitle(bookTitle: string): Promise<void> {
-  const deleteQuery = `
+  const deleteQuery:string = `
     delete from books where title like $1
   `;
-  const values = [`%${bookTitle}%`];
+  const values:string[] = [`%${bookTitle}%`];
   await pool.query(deleteQuery, values);
 }
 
 // Updates book information except reference number
 // Updates the 'updated_at' timestamp
+// the call argument was inspired from :
+// https://bobbyhadz.com/blog/typescript-object-destructuring-function-parameters
 async function updateBook({
                             reference,
                             title,
@@ -205,7 +210,7 @@ async function updateBook({
     set title=$2, author=$3, editor=$4, year=$5, price=$6, description=$7, cover=$8, stock=$9, updated_at=$10
     where reference=$1
   `;
-  const currentDate = new Date();
+  const currentDate:Date = new Date();
   const values = [reference, title, author, editor, year, price, description, cover, stock, currentDate];
   await pool.query(updateQuery, values);
 }
@@ -213,14 +218,14 @@ async function updateBook({
 // Decrements book stock by 1 if available
 // Throws error if book is out of stock or not found
 async function consumeBookStock(bookRef: number): Promise<void> {
-  const updateQuery = `
+  const updateQuery:string = `
     update books
     set stock = stock - 1, updated_at = $2
     where reference = $1 and stock > 0
   `;
-  const currentDate = new Date();
-  const values = [bookRef, currentDate];
-  const response = await pool.query(updateQuery, values);
+  const currentDate:Date = new Date();
+  const values:(number|Date)[] = [bookRef, currentDate];
+  const response:QueryResult = await pool.query(updateQuery, values);
   if (response.rowCount === 0) {
     throw new Error("Book out of stock or not found");
   }
@@ -228,24 +233,24 @@ async function consumeBookStock(bookRef: number): Promise<void> {
 
 // Increases book stock by specified amount
 async function replenishBookStock(bookRef: number, amount: number): Promise<void> {
-  const updateQuery = `
+  const updateQuery:string = `
     update books
     set stock = stock + $2, updated_at = $3
     where reference = $1
   `;
-  const currentDate = new Date();
-  const values = [bookRef, amount, currentDate];
+  const currentDate:Date = new Date();
+  const values:(number|Date)[] = [bookRef, amount, currentDate];
   await pool.query(updateQuery, values);
 }
 
 // Retrieves current stock level of a book
 // Throws error if book not found
 async function getBookStock(bookRef: number): Promise<number> {
-  const select = `
+  const select:string = `
     select stock from books where reference = $1
   `;
-  const values = [bookRef];
-  const response = await pool.query(select, values);
+  const values:number[] = [bookRef];
+  const response:QueryResult = await pool.query(select, values);
   if (response.rows.length === 0) {
     throw new Error("Book not found");
   }
@@ -255,8 +260,8 @@ async function getBookStock(bookRef: number): Promise<number> {
 
 // Retrieves all books from database
 async function showBooks(): Promise<Book[]> {
-  const select = `select * from books`;
-  const response = await pool.query(select);
+  const select:string = `select * from books`;
+  const response:QueryResult = await pool.query(select);
 
   // return all rows. Should include all books if no error
   return response.rows;
@@ -265,43 +270,46 @@ async function showBooks(): Promise<Book[]> {
 /////// ORDERS //////
 // Creates new order for user with optional status
 // Returns the created order details
-interface Order {
+export interface Order {
   id: number;
   user_id: number;
-  status: 'pending' | 'completed' | 'cancelled';
+  status: string;
   created_at: Date;
 }
 
-async function createOrder(userId: number, status: string = 'pending'): Promise<number> {
-  const insert = `
+export interface OrderResponse {
+  order: Order;
+}
+
+async function createOrder(userId: number, status: string = 'pending'): Promise<OrderResponse> {
+  const insert:string = `
     INSERT INTO orders (user_id,  status, created_at)
     VALUES ($1, $2, CURRENT_TIMESTAMP)
     RETURNING *
   `;
-  const values = [userId, status];
-  const response = await pool.query(insert, values);
-
-  // Should include order Id if no error
+  const values:(string|number)[] = [userId, status];
+  const response:QueryResult = await pool.query(insert, values);
+  // Should include order data if no error
   return response.rows[0];
 }
 
 // Removes an order from database
 async function deleteOrder(orderId: number): Promise<void> {
-  const deleteQuery = `
+  const deleteQuery:string = `
     DELETE FROM orders WHERE id = $1
   `;
-  const values = [orderId];
+  const values:number[] = [orderId];
   await pool.query(deleteQuery, values);
 }
 
 // Retrieves all orders for a specific user
 // Throws error if no orders found
 async function findOrderByCustomer(userId: number): Promise<Order[]> {
-  const select = `
+  const select:string = `
     SELECT * FROM orders WHERE user_id = $1
   `;
-  const values = [userId];
-  const response = await pool.query(select, values);
+  const values:number[] = [userId];
+  const response:QueryResult = await pool.query(select, values);
   if (response.rowCount === 0) {
     throw new Error("Order not found");
   }
@@ -313,11 +321,11 @@ async function findOrderByCustomer(userId: number): Promise<Order[]> {
 // Retrieves specific order by ID
 // Throws error if order not found
 async function findOrderById(orderId: number): Promise<Order> {
-  const select = `
+  const select:string = `
     SELECT * FROM orders WHERE id = $1
   `;
-  const values = [orderId];
-  const response = await pool.query(select, values);
+  const values:number[] = [orderId];
+  const response:QueryResult = await pool.query(select, values);
   if (response.rowCount === 0) {
     throw new Error("Order not found");
   }
@@ -328,12 +336,12 @@ async function findOrderById(orderId: number): Promise<Order> {
 // Updates order status and returns updated order
 // Throws error if order not found or update fails
 async function updateOrderStatus(orderId: number, status: string): Promise<Order> {
-  const update = `
+  const update:string = `
     UPDATE orders SET status = $2 WHERE id = $1
     RETURNING *
   `;
-  const values = [orderId, status];
-  const response = await pool.query(update, values);
+  const values:(string|number)[]  = [orderId, status];
+  const response:QueryResult = await pool.query(update, values);
   if (response.rowCount === 0) {
     throw new Error("Order not found or update failed");
   }
@@ -343,7 +351,7 @@ async function updateOrderStatus(orderId: number, status: string): Promise<Order
 /////// ORDER ITEMS //////
 // Adds item to an existing order
 // Returns the created order item details
-interface OrderItem {
+export interface OrderItem {
   id: number;
   order_id: number;
   book_id: number;
@@ -352,13 +360,13 @@ interface OrderItem {
 }
 
 async function addOrderItem(orderId: number, bookId: number, quantity: number, price: number): Promise<OrderItem> {
-  const insert = `
+  const insert:string = `
     INSERT INTO order_items (order_id, book_id, quantity, price)
     VALUES ($1, $2, $3, $4)
     RETURNING *
   `;
-  const values = [orderId, bookId, quantity, price];
-  const response = await pool.query(insert, values);
+  const values:number[]  = [orderId, bookId, quantity, price];
+  const response:QueryResult = await pool.query(insert, values);
 
   //  Should include order-item Id if no error
   return response.rows[0];
@@ -367,11 +375,11 @@ async function addOrderItem(orderId: number, bookId: number, quantity: number, p
 // Retrieves all items in a specific order
 // Throws error if no items found
 async function getOrderItemsByOrderId(orderId: number): Promise<OrderItem[]> {
-  const select = `
+  const select:string = `
     SELECT * FROM order_items WHERE order_id = $1
   `;
-  const values = [orderId];
-  const response = await pool.query(select, values);
+  const values:number[]  = [orderId];
+  const response:QueryResult = await pool.query(select, values);
   if (response.rowCount === 0) {
     throw new Error("Order Item not found");
   }
@@ -382,25 +390,28 @@ async function getOrderItemsByOrderId(orderId: number): Promise<OrderItem[]> {
 
 // Removes specific item from an order
 async function deleteOrderItem(orderItemId: number): Promise<void> {
-  const deleteQuery = `
+  const deleteQuery:string = `
     DELETE FROM order_items WHERE id = $1
   `;
-  const values = [orderItemId];
+  const values:number[] = [orderItemId];
   await pool.query(deleteQuery, values);
 }
 
 // Calculates total price of all items in an order
 // Returns 0 if order has no items
 async function computeOrderTotal(orderId: number): Promise<number> {
-  const query = `
+  const query:string = `
     SELECT SUM(quantity * price) AS total_price
     FROM order_items
     WHERE order_id = $1;
   `;
-  const values = [orderId];
+  const values:number[] = [orderId];
   try {
-    const result = await pool.query(query, values);
-    const totalPrice = result.rows[0].total_price;
+    const reponse:QueryResult = await pool.query(query, values);
+    if (reponse.rowCount === 0) {
+      throw new Error("Order not found");
+    }
+    const totalPrice:number = reponse.rows[0].total_price;
     return totalPrice || 0; // Return 0 if no items are found
   } catch (error) {
     console.error('Error computing order total:', error.message);
