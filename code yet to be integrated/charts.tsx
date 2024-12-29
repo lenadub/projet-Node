2518a74cd073ef8bc9cc55d4b543bbd6071f503f
lenadub <145775374+lenadub@users.noreply.github.com>
2024-12-29 21:05:25 +0100

    Update charts.tsx

--- code yet to be integrated/charts.tsx
+++ code yet to be integrated/charts.tsx
@@ -1,1 +1,30 @@
+import React from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 
+const Charts: React.FC = () => {
+    const options: Highcharts.Options = {
+        title: { text: "Sales Overview" },
+        xAxis: { categories: ["January", "February", "March", "April"] },
+        series: [
+            {
+                type: "line",
+                name: "Sales",
+                data: [150, 200, 250, 300],
+            },
+            {
+                type: "line",
+                name: "Expenses",
+                data: [100, 120, 180, 220],
+            },
+        ],
+    };
+
+    return (
+        <div>
+            <HighchartsReact highcharts={Highcharts} options={options} />
+        </div>
+    );
+};
+
+export default Charts;
